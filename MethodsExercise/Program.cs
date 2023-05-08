2bd6ff60ffa514d6f45df30e344c829c6f3d3947
Ryan Li <jijizaza999@gmail.com>
2023-05-08 11:40:51 -0700

    began user questions

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace MethodsExercise
+﻿using System.Drawing;
+
+namespace MethodsExercise
 {
     public class Program
     {
         static void Main(string[] args)
+
+
         {
-            Console.WriteLine("Hello, World!");
+            //   Your Name
+            //Your Favorite Color
+            //Your Favorite Animal
+            //Your Favorite Band
+
+
+            Console.WriteLine("Hello - What is your first name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hi, {userName} - What is your favorite color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine($"{color} is a awesome color, What is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine("Great, now what is your favorite band?");
+            var band = Console.ReadLine();
         }
     }
 }
