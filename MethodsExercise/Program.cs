9b0d411e0196513da8a68713b36fca878e0af380
Ryan Li <jijizaza999@gmail.com>
2023-05-11 09:25:26 -0700

    finished methods exercise

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -2,17 +2,31 @@
 
 namespace MethodsExercise
 {
-    public class Program
+    class Program
     {
-        static void Main(string[] args)
-
-
+        public static int Sum(int num1, int num2)
         {
-            //   Your Name
+            var answer = num1 + num2;
+            return answer;
+        }
+        public static int Multiply(int num1, int num2, int num3)
+        {
+
+            return num1 * num2 * num3;
+        }
+
+      static void Main(string[] args)
+        {
+
+            var amountOfCars = Sum(2, 6);
+            var blah = Multiply(60, 2, 4);
+
+       //------------Exercise 1 ----------------------
+            //Your Name
+            //Your Name
             //Your Favorite Color
             //Your Favorite Animal
             //Your Favorite Band
-
 
             Console.WriteLine("Hello - What is your first name?");
             var userName = Console.ReadLine();
@@ -24,7 +38,19 @@ namespace MethodsExercise
             var animal = Console.ReadLine();
 
             Console.WriteLine("Great, now what is your favorite band?");
-            var band = Console.ReadLine();
+            var band = Console.ReadLine();// Why does my terminal stopped printing from here and disappeared ???
+                                          //And I need Console.ReadLine() so Terminial wont escape and print the rest codes.
+
+            Console.WriteLine($"Thanks {userName}, Here is your profile.");
+            Console.WriteLine("--------------------------");
+
+
+            Console.WriteLine($"Name: {userName}");
+            Console.WriteLine($"Favorite Color: {color}");
+            Console.WriteLine($"Favorite Animal: {animal}");
+            Console.WriteLine($"Favorite Band:{band}");
+
         }
     }
-}
+
+        }
